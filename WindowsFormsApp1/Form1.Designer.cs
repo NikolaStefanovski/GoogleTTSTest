96e5aa04bc73051bf06887b218cdf121c5935618
NikolaStefanovski <nikolastefanovski992@gmail.com>
2018-12-07 16:47:03 +0100

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pickTokenFileBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.jsonTokenFilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(6, 19);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(621, 408);
+            this.textBox.TabIndex = 0;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(6, 3);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(153, 13);
+            this.label.TabIndex = 1;
+            this.label.Text = "Enter text for Google TTS";
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Location = new System.Drawing.Point(6, 433);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(153, 23);
+            this.convertBtn.TabIndex = 2;
+            this.convertBtn.Text = "Convert";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            this.convertBtn.Click += new System.EventHandler(this.ConvertBtn_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(15, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(641, 488);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox);
+            this.tabPage1.Controls.Add(this.label);
+            this.tabPage1.Controls.Add(this.convertBtn);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(633, 462);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Text";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.pickTokenFileBtn);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.jsonTokenFilePath);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(633, 462);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Settings";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pickTokenFileBtn
+            // 
+            this.pickTokenFileBtn.Location = new System.Drawing.Point(276, 33);
+            this.pickTokenFileBtn.Name = "pickTokenFileBtn";
+            this.pickTokenFileBtn.Size = new System.Drawing.Size(75, 23);
+            this.pickTokenFileBtn.TabIndex = 2;
+            this.pickTokenFileBtn.Text = "Choose file";
+            this.pickTokenFileBtn.UseVisualStyleBackColor = true;
+            this.pickTokenFileBtn.Click += new System.EventHandler(this.PickTokenFileBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "JSON token file";
+            // 
+            // jsonTokenFilePath
+            // 
+            this.jsonTokenFilePath.Location = new System.Drawing.Point(6, 35);
+            this.jsonTokenFilePath.Name = "jsonTokenFilePath";
+            this.jsonTokenFilePath.ReadOnly = true;
+            this.jsonTokenFilePath.Size = new System.Drawing.Size(264, 20);
+            this.jsonTokenFilePath.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 512);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Google TTS";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button convertBtn;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox jsonTokenFilePath;
+        private System.Windows.Forms.Button pickTokenFileBtn;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,105 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.TextToSpeech.V1;
+using Grpc.Auth;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static string settingsFileName = "settings.ini";
+
+        string jsonTokenFile = "C:\\Users\\Nikola\\Temp\\My First Project-039858c06c3d.json";
+        string outputFile = "C:\\Users\\Nikola\\Temp\\output.mp3";
+        List<string> voices;
+        GoogleCredential credential;
+        Channel channel;
+        TextToSpeechClient client;
+        VoiceSelectionParams voiceSelection;
+        SynthesisInput input;
+        AudioConfig audioConfig;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            credential = GoogleCredential.FromFile(jsonTokenFile)
+                .CreateScoped(TextToSpeechClient.DefaultScopes);
+            channel = new Channel(TextToSpeechClient.DefaultEndpoint.ToString(), credential.ToChannelCredentials());
+            client = TextToSpeechClient.Create(channel);
+
+            if (File.Exists(settingsFileName))
+            {
+                using (StreamReader reader = new StreamReader(settingsFileName))
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+
+                    }
+                }
+
+
+            }
+            else
+            {
+
+            }
+
+
+        }
+
+        private void LoadSettings()
+        {
+
+        }
+
+        private void ConvertBtn_Click(object sender, EventArgs e)
+        {
+            
+
+            // The input to be synthesized, can be provided as text or SSML.
+            //var input = new SynthesisInput
+            //{
+            //    Text = textBox.Text
+            //};
+
+            // Build the voice request.
+            //var voiceSelection = new VoiceSelectionParams
+            //{
+            //    LanguageCode = "en-US",
+            //    Name = "en-US-Wavenet-A",
+            //    SsmlGender = SsmlVoiceGender.Neutral
+            //};
+
+            // Specify the type of audio file.
+            //var audioConfig = new AudioConfig
+            //{
+            //    AudioEncoding = AudioEncoding.Mp3
+            //};
+
+            // Perform the text-to-speech request.
+            var response = client.SynthesizeSpeech(input, voiceSelection, audioConfig);
+
+            // Write the response to the output file.
+            using (var output = File.Create(outputFile))
+            {
+                response.AudioContent.WriteTo(output);
+            }
+        }
+
+        private void PickTokenFileBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
